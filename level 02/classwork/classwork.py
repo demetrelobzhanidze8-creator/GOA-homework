eea4576843fc2de93145858039245ee938552a8b
demetrelobzhanidze8-creator <demetrelobzhanidze8@gmail.com>
2026-02-08 23:02:22 +0300

    second commit

---
+++ level 02/classwork/classwork.py
@@ -0,0 +1,29 @@
+# + with this comand you can add lains and numbers
+# - yuo can Subtraction numbers
+# * you can Multiplication numbers adn teqst
+# / you can Division numbers
+# // you can Division numbers withaut float
+# % you can get remainder of division
+
+print(10 +3)
+print(10 -3)
+print(10 *3)
+print(10 /3)
+print(10 //3)
+print(10 %3)
+
+# თუ არ ვცდები კონკარაცია არის მაგ: print(დემეს + ( ) + ლობჟანიძე)
+
+name = "demetre"
+surname = "lobzhanidze"
+print(name + " " + surname)
+
+nam1 = 15
+nam2 = 10
+print(nam1 + nam2)
+
+name1 = "demetre"
+height = 1.70
+age = 16
+
+print(f'my name is {name1}, and my height is {height}, my age is {age}')
---
+++ level 02/homework/homework.py
@@ -0,0 +1,45 @@
+# როდესაც რიცხვი ბრჭყალებშია მოგცეული "" ეს რიცხვი ხდება stringi და მას ეკარგება ყველანაირი მათემატიკური მნიშვნელობა.
+# და ჩვენ რიცხვს ბრჭყალების გარეშე დავწერთ ჩვენ შეგვიძლია მასზე ნებისმიერი მათემატიკური ოპერაცია ჩავატაროთ.
+
+
+num1 = 50
+num2 = 50
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+
+num3 = 57
+num4 = 4
+
+print(num3 + num4)
+print(num3 - num4)
+print(num3 * num4)
+print(num3 / num4)
+print(num3 // num4)
+print(num3 % num4)
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+x = 10
+x = x + 5
+x = x * 2
+
+print(x)
+
+# რადგან როდესაც ჩვენ ვწერთ x = x + 5 ეს ნიშნავს რომ ჩვენ ვიღებთ x ის მნიშვნელობას და ვამატებთ მას 5 და შემდეგ ვანიჭებთ ამ ახალ მნიშვნელობას ისევ x ზე.
+# იგივე ხდება შემდეგი ხაზის დროს როდესაც ჩვენ ვწერთ x = x * 2, ჩვენ ვიღებთ x ის ახალ მნიშვნელობას და ვამრავლებთ მას 2 ით და შემდეგ ვანიჭებთ ამ ახალ მნიშვნელობას ისევ x ზე.
+
+
+balance = 100
+balance = balance - 30
+balance = balance + 50
+balance = balance * 2
+print(balance)
+
+
+
+
+print('Group 96 is the best' * 96)
