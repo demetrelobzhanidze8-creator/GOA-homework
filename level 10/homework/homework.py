a7171c265b5bbc560ddac00ced6e7bbfb907a3ec
demetrelobzhanidze8-creator <demetrelobzhanidze8@gmail.com>
2026-03-11 21:22:12 +0300

    second commit

---
+++ level 10/homework/homework.py
@@ -0,0 +1,34 @@
+
+
+for i in range(50, 100):
+    print(i)
+
+print("------------------")
+
+for i in range(0, 101, 2):
+    print(i)
+
+print("------------------")
+
+for i in range(101, 150, 2):
+    print(i)
+
+print("------------------")
+
+num = 0
+while num < 51:
+    print(num)
+    num += 1
+
+print("------------------")
+
+num2 = 20
+while num2 < 61:
+    print(num2)
+    num2 += 5
+
+
+
+surname = input("What is your surname? ")
+for i in surname:
+    print(i)
---
+++ level 10/homework/homework1.py
@@ -0,0 +1,19 @@
+
+
+soda = 300
+while soda > -1:
+    print("You have bought the soda: " + str(soda))
+    soda -= 2
+print("Out of stock")
+
+
+
+for i in range(50, 20, -3):
+    print(i)
+
+print("------------------")
+
+num = 150
+while num > 0:
+    print(num)
+    num -= 1
