f12d717f3b4abc731bca797f68283eb1f7272cb0
demetrelobzhanidze8-creator <demetrelobzhanidze8@gmail.com>
2026-03-26 14:55:14 +0300

    second commit

---
+++ level 14/homework/homework.py
@@ -0,0 +1,52 @@
+
+
+# N3
+phrase = "Python Programming"
+
+ph1 = ( phrase[:6])
+print(ph1)
+
+ph2 = ( phrase[-11:])
+print(ph2)
+
+ph3 = ( phrase[::3])
+print(ph3)
+
+
+# N4
+text = "PythonSlicing"
+
+t1 = ( text[:6])
+print(t1)
+
+t2 = ( text[::-1])
+print(t2)
+
+t3 = ( text[::2])
+print(t3)
+
+
+# N5
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+l1 = ( letters[1::2])
+print(l1)
+
+l2 = ( letters[2:7:2])
+print(l2)
+
+l3 = ( letters[4::-1])
+print(l3)
+
+
+# N6
+sentence = "Slicing makes Python powerful"
+
+s1 = ( sentence[14:20])
+print(s1)
+
+s2 = ( sentence[29:20:-1])
+print(s2)
+
+s3 = ( sentence[::3])
+print(s3)
