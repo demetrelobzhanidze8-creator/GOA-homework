623218883bcfe28703e457d5d381fa4cfba638e2
demetrelobzhanidze8-creator <demetrelobzhanidze8@gmail.com>
2026-03-30 00:49:45 +0300

    second commit

---
+++ level 16/classwork/classwork.py
@@ -0,0 +1,28 @@
+
+
+text = input('Enter a text: ')
+
+apper= text 
+
+if text .apper :
+    print('upper leter')
+else:
+    print('lower leter')
+
+
+
+
+s = input("Enter a text: ")
+
+l = input("Enter a leter: ")
+
+print(s .find(l))
+
+
+fruits = ['apple','banana','peach','pineapple']
+
+fruits.insert(11,12)
+fruits.insert(12,13)
+fruits.insert(13,14)
+
+print(len(fruits))
